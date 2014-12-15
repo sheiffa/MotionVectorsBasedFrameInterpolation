b4b55ae357f9be277169d6234e8cc1e7122e5139
xvcine <carltonz@hotmail.com>
2014-12-15 06:21:28 +0100

    Motion Vectors update with Error Vectors

--- BlockFpsWithCorrectionVectors.h
+++ BlockFpsWithCorrectionVectors.h
@@ -6,16 +6,12 @@
 class BlockFpsWithCorrectionVectors : public MVBlockFps
 {
 private:
-	PClip errorVectors;
+	MVClip errorVectors;
 	int fpsMultiplier;
-
-	void ResultBlock(BYTE *pDst, int dst_pitch, const BYTE * pMCB, int MCB_pitch, const BYTE * pMCF, int MCF_pitch,
-		const BYTE * pRef, int ref_pitch, const BYTE * pSrc, int src_pitch, BYTE *maskB, int mask_pitch, BYTE *maskF,
-		BYTE *pOcc, int nBlkSizeX, int nBlkSizeY, int time256, int mode);
-
 
 public:
 	BlockFpsWithCorrectionVectors(PClip source, PClip super, PClip backwardVectors, PClip forwardVectors, PClip errorVectors, int sourceFps, int fpsMultiplier, int mode, IScriptEnvironment* env);
 	~BlockFpsWithCorrectionVectors();
+	PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment* env);
 };
 
